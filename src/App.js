d3e74ef508c5589df132521cc28dcbdf521ada25
Thomas Collardeau <thomas@collardeau.com>
2018-04-05 11:30:07 +0200

    add loaded state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    loaded: false
+  };
+
   render() {
+    if (!this.state.loaded) return <div>Loading...</div>;
     return (
       <div>
         <h1>Todos</h1>
