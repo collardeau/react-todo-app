5008e3e704ecc39b45791be451ef5e1b102bf1f2
Thomas Collardeau <thomas@collardeau.com>
2018-04-06 08:27:03 +0200

    add mock db

---
+++ src/mockDB.js
@@ -0,0 +1,54 @@
+function createTodo(todo) {
+  const id = Math.floor(Math.random() * Math.floor(999999)) + '';
+  return {
+    [id]: {
+      ...todo,
+      done: false,
+      ts: Date.now()
+    }
+  };
+}
+
+// our mock DB initiated with 2 todos
+let db = {
+  todos: {
+    ...createTodo({ title: 'learn react' }),
+    ...createTodo({ title: 'master react' })
+  }
+};
+
+let refreshTodos = () => {};
+
+function syncTodos(cb) {
+  refreshTodos = (delay = 0) => {
+    setTimeout(() => {
+      cb(db.todos);
+    }, delay);
+  };
+  refreshTodos(500);
+}
+
+function addTodo(todo) {
+  db.todos = {
+    ...db.todos,
+    ...createTodo(todo)
+  };
+  refreshTodos();
+}
+
+function updateTodo(id, changes) {
+  db.todos = {
+    ...db.todos,
+    [id]: {
+      ...db.todos[id],
+      ...changes
+    }
+  };
+  refreshTodos();
+}
+
+export default {
+  syncTodos,
+  addTodo,
+  updateTodo
+};
