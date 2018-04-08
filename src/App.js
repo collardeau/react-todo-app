41f7355b15d7b12f4992939dffa01e8f64a6b963
Thomas Collardeau <thomas@collardeau.com>
2018-04-08 07:03:04 +0200

    render todos from mock db

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
+import db from './mockDB';
+
+const toArray = obj => Object.keys(obj).map(key => ({ ...obj[key], id: key }));
+const sortByTs = todos => toArray(todos).sort((a, b) => b.ts - a.ts);
 
 class App extends Component {
   state = {
+    todos: {},
+    todoList: [],
     loaded: false
   };
+
+  actions = {
+    syncTodos: () => {
+      db.syncTodos(todos => {
+        this.setState({
+          todos,
+          loaded: true,
+          todoList: sortByTs(todos)
+        });
+      });
+    }
+  };
+
+  componentDidMount() {
+    this.actions.syncTodos();
+  }
 
   render() {
     if (!this.state.loaded) return <div>Loading...</div>;
     return (
       <div>
         <h1>Todos</h1>
+        {this.state.todoList.map(todo => <div key={todo.id}>{todo.title}</div>)}
       </div>
     );
   }
